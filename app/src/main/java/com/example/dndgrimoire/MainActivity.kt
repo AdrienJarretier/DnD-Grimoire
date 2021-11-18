29464ee84d950a6fce7ac5c7e60edab9d6de9ca9
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-18 15:31:48 +0100

    display an example of spell card

--- app/src/main/java/com/example/dndgrimoire/MainActivity.kt
+++ app/src/main/java/com/example/dndgrimoire/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.dndgrimoire
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val subtitle = getString(R.string.subtitle)
+        val levelValue = resources.getInteger(R.integer.level_value)
+
+        Log.d(levelValue.toString(), levelValue.toString())
+
+        val magicSchool = getString(R.string.magic_school)
+
+        val subtitleTextView = findViewById<TextView>(R.id.subtitle)
+        subtitleTextView.text = String.format(subtitle, levelValue, magicSchool)
+
     }
 }
