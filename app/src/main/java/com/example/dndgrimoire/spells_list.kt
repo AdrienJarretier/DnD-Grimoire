ed66754d4d228da2a116d16f8a220111ec8bddc9
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-18 18:47:54 +0100

    list button intented to open list of spells activity

---
+++ app/src/main/java/com/example/dndgrimoire/spells_list.kt
@@ -0,0 +1,11 @@
+package com.example.dndgrimoire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class spells_list : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spells_list)
+    }
+}
