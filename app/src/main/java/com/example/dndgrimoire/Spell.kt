f44c4059af200379aeeb9acd94a6366af76c7db5
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-19 14:11:51 +0100

    spell attributes as string except level

--- app/src/main/java/com/example/dndgrimoire/Spell.kt
+++ app/src/main/java/com/example/dndgrimoire/Spell.kt
@@ -11,15 +11,15 @@ data class Spell(
     @ColumnInfo val spell_name: String,
     @ColumnInfo val level: Int,
     @ColumnInfo val school: String,
-    @ColumnInfo val cast_time: Int,
-    @ColumnInfo val range: Float,
+    @ColumnInfo val cast_time: String,
+    @ColumnInfo val range: String,
     @ColumnInfo val components: String,
-    @ColumnInfo val duration: Int,
+    @ColumnInfo val duration: String,
     @ColumnInfo val description: String
 ){
     constructor(spell_name: String,level: Int,
-                school: String,cast_time: Int,
-                range: Float,components: String,
-                duration: Int,description: String)
+                school: String,cast_time: String,
+                range: String,components: String,
+                duration: String,description: String)
             :this(null, spell_name, level, school, cast_time, range, components, duration, description)
 }
