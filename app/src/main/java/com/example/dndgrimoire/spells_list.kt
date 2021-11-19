80d67d11d87266a16840d3dbc833db917359888c
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-19 13:57:39 +0100

    dynamic textView in spells list fragment

--- app/src/main/java/com/example/dndgrimoire/spells_list.kt
+++ app/src/main/java/com/example/dndgrimoire/spells_list.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +36,15 @@ class spells_list : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spells_list, container, false)
+
+        val rootView : LinearLayout =
+            inflater.inflate(R.layout.fragment_spells_list, container, false) as LinearLayout
+
+        val text = TextView(context)
+        text.text = "dynamic"
+        rootView.addView(text)
+
+        return rootView
     }
 
     companion object {
