3aad509d9557332c8b6eb0b0a20907a5f3f9e6cf
Adrien Jarretier-Yuste <jarretier.adrien@gmail.com>
2021-11-30 18:54:25 +0100

    fixed playerSheet typo

--- app/src/main/java/com/example/dndgrimoire/PlayerSheet.kt
+++ app/src/main/java/com/example/dndgrimoire/PlayerSheet.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PlyaerSheet.newInstance] factory method to
+ * Use the [PlayerSheet.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PlyaerSheet : Fragment() {
+class PlayerSheet : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class PlyaerSheet : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PlyaerSheet().apply {
+            PlayerSheet().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
